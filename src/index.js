4e54456142805b9c5c44bd97536e91c3f3b86dc4
Charlie Cochran <charlie.cochran@gmail.com>
2019-09-19 11:20:47 -0500

    Add babel build

--- src/index.js
+++ src/index.js
@@ -1,5 +1,65 @@
-import useAsyncCallback from './useAsyncCallback';
-import useAsyncEffect from './useAsyncEffect';
+import { useCallback, useEffect, useState } from 'react';
+
+const useAsyncCallback = (asyncCallback, dependencies) => {
+	const [result, setResult] = useState({ loading: false });
+	const callback = useCallback(async function callback(...args) {
+		setResult({ loading: true });
+		try {
+			setResult({
+				data: await asyncCallback(...args),
+				loading: false,
+			});
+		} catch (error) {
+			setResult({
+				error,
+				loading: false,
+				retry: () => callback(...args),
+			});
+		}
+	}, dependencies);
+
+	return [callback, result];
+};
+
+const runAsyncEffect = async (asyncEffect, setState, refetch) => {
+	let cancelled = false;
+
+	try {
+		const data = await asyncEffect();
+
+		if (!cancelled) {
+			setState({
+				data,
+				loading: false,
+				refetch,
+			});
+		}
+	} catch (error) {
+		if (!cancelled) {
+			setState({
+				error,
+				loading: false,
+				refetch,
+			});
+		}
+	}
+
+	return () => {
+		cancelled = true;
+	};
+};
+
+const useAsyncEffect = (asyncEffect, dependencies = []) => {
+	const [timestamp, setTimestamp] = useState();
+	const [state, setState] = useState({ loading: true });
+
+	useEffect(() => {
+		setState({ loading: true });
+		runAsyncEffect(asyncEffect, setState, () => setTimestamp(Date.now()));
+	}, [timestamp, ...dependencies]);
+
+	return state;
+};
 
 export { useAsyncCallback, useAsyncEffect };
 
