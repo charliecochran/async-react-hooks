9b3b3c8d3e878012388402b0e884f4b393b408b8
Charlie Cochran <charlie.cochran@gmail.com>
2019-10-05 18:37:50 -0500

    Add typescript definitions

---
+++ src/index.d.ts
@@ -0,0 +1,30 @@
+interface Result<TData = any, TError = Error | string> {
+	data?: TData;
+	error?: TError;
+	loading: boolean;
+	refetch?(): void;
+}
+
+type AnyFunction = (...args: any[]) => any;
+type Callback<TParams = any[]> = (...args: TParams) => void;
+
+export function useAsyncCallback(asyncCallback: AnyFunction, deps?: ReadonlyArray<any>): [Callback, Result];
+export function useAsyncCallback<TData>(
+	asyncCallback: AnyFunction,
+	deps?: ReadonlyArray<any>,
+): [Callback, Result<TData>];
+export function useAsyncCallback<TData, TError>(
+	asyncCallback: AnyFunction,
+	deps?: ReadonlyArray<any>,
+): [Callback, Result<TData, TError>];
+export function useAsyncCallback<TData, TError, TCallback extends AnyFunction>(
+	asyncCallback: TCallback,
+	deps?: ReadonlyArray<any>,
+): [Callback<Parameters<TCallback>>, Result<TData, TError>];
+
+export function useAsyncEffect(asyncEffect: AnyFunction, deps?: ReadonlyArray<any>): Result;
+export function useAsyncEffect<TData>(asyncEffect: AnyFunction, deps?: ReadonlyArray<any>): Result<TData>;
+export function useAsyncEffect<TData, TError>(
+	asyncEffect: AnyFunction,
+	deps?: ReadonlyArray<any>,
+): Result<TData, TError>;
